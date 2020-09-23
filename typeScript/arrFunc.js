211392bad903a4dd5d7519f1a609b46d408720c3
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-09-23 22:38:42 +0530

    Add files via upload

---
+++ typeScript/arrFunc.js
@@ -0,0 +1,5 @@
+var log = function (messsage) {
+    console.log(messsage);
+};
+var doLog = function (message) { return console.log(message); };
+doLog("Hello");
---
+++ typeScript/arrFunc.ts
@@ -0,0 +1,6 @@
+let log = function (messsage) {
+  console.log(messsage);
+};
+
+let doLog = (message) => console.log(message);
+doLog("Hello");
