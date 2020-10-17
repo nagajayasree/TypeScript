86b0221673f8b1465e21a2996a092ae3b0a7b181
nagajayasree <52131854+nagajayasree@users.noreply.github.com>
2020-10-18 00:50:20 +0530

    Add files via upload

---
+++ src/Code/useEffect.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+interface Props {
+  //   name: string;
+  //   city: string;
+}
+
+const UseEffectComp: React.FC<Props> = ({}) => {
+  const [name, setName] = React.useState<string>("");
+  const [address, setAddress] = React.useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(name);
+  };
+
+  const onNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const onAddrChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAddress(e.target.value);
+  };
+
+  React.useEffect(() => {
+    console.log("Component Mounted");
+    return () => {
+      console.log("Component will be unmounted");
+    };
+  }, []);
+
+  React.useEffect(() => {
+    console.log(`Any state changed Name: ${name},Address:${address}`);
+  });
+
+  React.useEffect(() => {
+    console.log(`Name Changed: ${name}`);
+  }, [name]);
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="name"
+        value={name}
+        onChange={onNameChange}
+      />
+      <input
+        type="text"
+        placeholder="address"
+        value={address}
+        onChange={onAddrChange}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default UseEffectComp;
