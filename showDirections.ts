c2862ed26662e70d1a442252af09324959440c90
Jaysree <js@ramaka.com>
2020-12-18 19:29:57 +0530

    added showDirectionFiles

---
+++ showDirections.ts
@@ -0,0 +1,24 @@
+var myButton = document.createElement("button");
+function showDirection(directions) {
+    var randomDir = Math.floor(Math.random() * directions.length);
+    document.write(directions[randomDir]);
+}
+var directions = ["e", "w", "n", "s"];
+myButton.innerHTML = "Show Direction";
+document.body.appendChild(myButton);
+myButton.addEventListener("click", function (e) {
+    showDirection(directions);
+});
+// const button = document.createElement("button");
+// window.document.appendChild(button);
+// document.body.appendChild(button);
+// myButton.onclick = showDirection(directions);
+// window.document.appendChild(myButton);
+// window.onload = () => {
+//   var docContent = document.getElementById("content");
+//   var button = document.createElement("button");
+//   button.innerText = "Get Direction";
+//   //   button.onclick = showDirection;
+//   docContent.appendChild(button);
+// };
+// showDirection(directions);
