ba6a3ee6afc9302beba35b72b652d6ddf017ef78
Jaysree <js@ramaka.com>
2020-12-23 23:01:46 +0530

    sqEveryDigit

---
+++ sqEveryDigit.js
@@ -0,0 +1,13 @@
+function sqEveryDigit(number) {
+    console.log(number);
+    var numStr = number.toString();
+    var digits = [];
+    for (var i = 0; i < numStr.length; i++) {
+        var square = numStr[i] * numStr[i];
+        digits.push(square);
+    }
+    var result = digits.join(" ");
+    console.log(typeof result);
+    console.log(result);
+}
+sqEveryDigit(121);
---
+++ sqEveryDigit.ts
@@ -0,0 +1,13 @@
+function sqEveryDigit(number) {
+  console.log(number);
+  let numStr = number.toString();
+  let digits = [];
+  for (let i = 0; i < numStr.length; i++) {
+    let square = numStr[i] * numStr[i];
+    digits.push(square);
+  }
+  let result = digits.join(" ");
+  console.log(result);
+}
+
+sqEveryDigit(121);
